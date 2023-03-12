3c03464e90ea89149d235e3e7265d5f773967bd6
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-03-12 17:03:40 +0000

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Tony Chaidinis on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
