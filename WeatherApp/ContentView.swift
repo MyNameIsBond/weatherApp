eab5f668533b9d08e8207c86f2f39b6a1536f025
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-03-13 21:47:51 +0000

    api call in images, on the go

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -3,13 +3,23 @@ import SwiftUI
 import CoreLocation
 struct ContentView: View {
   @StateObject private var locationManager = LocationManager()
-  
+  var apiClient = APIClient.shared
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
           Text("Last location: \(locationManager.lastLocation?.coordinate.latitude ?? 0), \(locationManager.lastLocation?.coordinate.longitude ?? 0)")
+          Button("Show") {
+            apiClient.getWeatherData(forLatitude: locationManager.lastLocation?.coordinate.latitude ?? 0, longitude: locationManager.lastLocation?.coordinate.longitude ?? 0, apiKey: apiClient.apiKey) { (result) in
+                switch result {
+                case .success(let data):
+                    print("Data: \(data)")
+                case .failure(let error):
+                    print("Error: \(error)")
+                }
+            }
+          }
         }
         .padding()
         .onAppear {
--- WeatherApp/Model/WeatherApi.swift
+++ WeatherApp/Model/WeatherApi.swift
@@ -2,50 +2,51 @@
 import Foundation
 
 class APIClient {
+  static let shared = APIClient()
   
   let apiKey = "81cfeabb4b9ed6029335733752871236"
+  
+  func fetchData(forLatitude latitude: Double, longitude: Double, exclude: String?, apiKey: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
     
-    func fetchData(forLatitude latitude: Double, longitude: Double, exclude: String?, apiKey: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
-        
-        var urlComponents = URLComponents(string: "https://api.openweathermap.org/data/3.0/onecall")!
-        urlComponents.queryItems = [
-            URLQueryItem(name: "lat", value: String(latitude)),
-            URLQueryItem(name: "lon", value: String(longitude)),
-            URLQueryItem(name: "exclude", value: exclude),
-            URLQueryItem(name: "appid", value: apiKey)
-        ].filter { $0.value != nil }
-        
-        guard let url = urlComponents.url else {
-            let error = NSError(domain: "com.example.app", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
-            completion(.failure(error))
-            return
-        }
-        
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard let data = data else {
-                let error = NSError(domain: "com.example.app", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received"])
-                completion(.failure(error))
-                return
-            }
-            
-            do {
-                let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] ?? [:]
-                completion(.success(json))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-        
-        task.resume()
+    var urlComponents = URLComponents(string: "https://api.openweathermap.org/data/2.5/weather")!
+    urlComponents.queryItems = [
+      URLQueryItem(name: "lat", value: String(latitude)),
+      URLQueryItem(name: "lon", value: String(longitude)),
+      URLQueryItem(name: "exclude", value: exclude),
+      URLQueryItem(name: "appid", value: apiKey)
+    ].filter { $0.value != nil }
+    
+    guard let url = urlComponents.url else {
+      let error = NSError(domain: "com.example.app", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+      completion(.failure(error))
+      return
     }
     
-    func getWeatherData(forLatitude latitude: Double, longitude: Double, apiKey: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
-        fetchData(forLatitude: latitude, longitude: longitude, exclude: nil, apiKey: apiKey, completion: completion)
+    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+      
+      if let error = error {
+        completion(.failure(error))
+        return
+      }
+      
+      guard let data = data else {
+        let error = NSError(domain: "com.example.app", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received"])
+        completion(.failure(error))
+        return
+      }
+      
+      do {
+        let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] ?? [:]
+        completion(.success(json))
+      } catch {
+        completion(.failure(error))
+      }
     }
+    
+    task.resume()
+  }
+  
+  func getWeatherData(forLatitude latitude: Double, longitude: Double, apiKey: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
+    fetchData(forLatitude: latitude, longitude: longitude, exclude: nil, apiKey: apiKey, completion: completion)
+  }
 }
